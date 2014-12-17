3b6ace0764e0fe3439e88c00ae88691b9ba2a8dc
Amrita Mazumdar <amritamaz@gmail.com>
2014-12-16 18:33:34 -0800

    removed wrong comments

--- blank.c
+++ blank.c
@@ -1,12 +1,7 @@
 #include "stdio.h"
 #include "msp430.h"
 
-/**
- * This implements the user application and should never return
- *
- * Must call WISP_init() in the first line of main()
- * Must call WISP_doRFID() at some point to start interacting with a reader
- */
+
 int main(void){
 	unsigned int i;
 	for (i = 100; i > 0; i--){
